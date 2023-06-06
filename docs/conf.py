af1f165197f66e1189c55617d6abc4f743bd4823
Wally <107321197+oramirez2025@users.noreply.github.com>
2023-06-06 13:35:22 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -84,8 +84,6 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = "images/AirLabLogo.png"
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
