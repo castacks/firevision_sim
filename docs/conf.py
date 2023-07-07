994bf5c225f9f8fd430548be2aa3fd778482d968
Wally <107321197+oramirez2025@users.noreply.github.com>
2023-07-07 11:40:56 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 import subprocess
 subprocess.check_call([sys.executable, "-m", "pip", "install", "sphinxcontrib-youtube"])
-from sphinxcontrib.youtube import __version__
+import sphinxcontrib.youtube
 sys.path.insert(0, os.path.abspath('../source'))
 
 
